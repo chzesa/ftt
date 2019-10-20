a400bab6d754a5df7af2e6343f99711f757e5af0
chzesa <chzesa+git@gmail.com>
2019-10-20 09:26:46 +0300

    Reformat api enums

--- src/js/api.js
+++ src/js/api.js
@@ -1,22 +1,22 @@
 const SIGNAL_TYPE = {
-	dragDrop: 0,
-	selectAll: 1,
-	deselectAll: 2
+	dragDrop: 0
+	, selectAll: 1
+	, deselectAll: 2
 };
 
 const MSG_TYPE = {
-	Register: 0,
-	DropMoving: 1,
-	DropParenting: 2,
-	GetSelection: 3,
-	SetSelectionSource: 4,
-	OnActivated: 5,
-	OnCreated: 6,
-	OnMoved: 7,
-	OnRemoved: 8,
-	OnUpdated: 9,
-	UpdateSidebarContextMenu: 10,
-	Signal: 11,
-	GetSelectionSource: 12,
-	SessionsValueUpdated: 13
+	Register: 0
+	, DropMoving: 1
+	, DropParenting: 2
+	, GetSelection: 3
+	, SetSelectionSource: 4
+	, OnActivated: 5
+	, OnCreated: 6
+	, OnMoved: 7
+	, OnRemoved: 8
+	, OnUpdated: 9
+	, UpdateSidebarContextMenu: 10
+	, Signal: 11
+	, GetSelectionSource: 12
+	, SessionsValueUpdated: 13
 };
